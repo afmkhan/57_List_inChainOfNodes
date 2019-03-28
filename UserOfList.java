25b09fc3961bac1c4b4ac5167aad02aab2ec524a
afmkhan <47547127+afmkhan@users.noreply.github.com>
2019-03-28 12:00:55 -0400

    homework upload 3/28

--- UserOfList.java
+++ UserOfList.java
@@ -25,5 +25,52 @@ public class UserOfList {
         }
         System.out.println( "populated list: " + list
                           + System.lineSeparator());
+						  
+						  
+		// test get(index) 
+		System.out.println( "testing get()");
+		System.out.println( list);
+		System.out.println( "index 0: " + list.get(0));
+		System.out.println( "index 2: " + list.get(2));
+		System.out.println();
+
+		// test add(index, val) 
+		System.out.println( "testing add(index, val)");
+		System.out.println( list);
+		System.out.println( "result of adding to index 1: ");
+		list.add(1, "testing");
+		System.out.println( list);
+		System.out.println( "result of adding to end ");
+		list.add(5, 2019);
+		System.out.println( list);
+		System.out.println( "result of adding to beginning ");
+		list.add(0, "classOf");
+		System.out.println( list + System.lineSeparator());
+
+		// test set(index, val) 
+		System.out.println( "testing set(index)");
+		System.out.println( list);
+		System.out.println( "result of setting index2 to true ");
+		list.set(2, true);
+		System.out.println( list);
+		System.out.println( "result of setting beginning to 10000 ");
+		list.set(0, 10000);
+		System.out.println( list);
+		System.out.println( "result of setting end to STUY ");
+		list.set(6, "STUY");
+		System.out.println( list + System.lineSeparator());
+
+		// test remove(index) 
+		System.out.println( "testing remove(index)");
+		System.out.println( list);
+		System.out.println( "result of removing index 3: ");
+		list.remove(3);
+		System.out.println( list);
+		System.out.println( "result of removing end ");
+		list.remove(5);
+		System.out.println( list);
+		System.out.println( "result of removing beginning ");
+		list.remove(0);
+		System.out.println( list + System.lineSeparator());
     }
 }
